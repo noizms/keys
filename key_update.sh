b06c50ed29b3b7ec5c7200bb9db26eea84a399d7
noizms <noizms@gmail.com>
2020-05-27 16:26:40 +0900

    add altair

--- key_update.sh
+++ key_update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat ./*.pub > ~/.ssh/authorized_keys
+cat ./*.pub >> ~/.ssh/authorized_keys
 echo "keyring update done. Here is keyring lists..."
 cat ~/.ssh/authorized_keys | awk '{print $3}'
