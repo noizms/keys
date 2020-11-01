10f25fb96b876028ef6efc6eebe3ca7ca0ad9621
noizms <36431445+noizms@users.noreply.github.com>
2020-11-01 16:21:42 +0900

    script update. sort + unique keys / make ~.old backup file

--- key_update.sh
+++ key_update.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
-cat ./*.pub >> ~/.ssh/authorized_keys
+
+LASTPATH=`pwd`
+SHPATH=`dirname $0`
+
+# go working path
+cd $SHPATH
+
+
+# make backup
+cp  ~/.ssh/authorized_keys  ~/.ssh/authorized_keys.old
+
+# generate tmpfile and add keys, clean tmpfile
+cp  ~/.ssh/authorized_keys  ~/.ssh/authorized_keys.tmp
+cat ./*.pub >> ~/.ssh/authorized_keys.tmp
+cat  ~/.ssh/authorized_keys.tmp | sort -u > ~/.ssh/authorized_keys
+rm -f =/.ssh/authorized_keys.tmp
+
+# display
+# FIXME: needed error handling...
 echo "keyring update done. Here is keyring lists..."
 cat ~/.ssh/authorized_keys | awk '{print $3}'
+
+
+# get back last path
+cd $LASTPATH
